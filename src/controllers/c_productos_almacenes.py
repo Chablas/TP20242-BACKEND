81598ac68a44fa9e71ebafb321b7170571ffe48b
Christian Alvarez <116127459+Chablas@users.noreply.github.com>
2024-11-19 17:52:13 -0500

    añadir stock a almacenes

--- src/controllers/c_productos_almacenes.py
+++ src/controllers/c_productos_almacenes.py
@@ -1,6 +1,7 @@
 from src.api.db.models.m_productos_almacenes import Producto_Almacen as Producto_AlmacenModel
 from src.api.db.schemas.s_producto_almacen import ProductoAlmacenCreate, ProductoAlmacenCompleto, ProductoAlmacenResponse
 from src.api.db.models.m_productos import Producto as ProductoModel
+from src.api.db.models.m_bienes import Bien as BienModel
 from src.api.db.models.m_almacenes import Almacen as AlmacenModel
 from fastapi import HTTPException, status
 
@@ -15,7 +16,7 @@ def c_aumentar_stock(db, entrada:ProductoAlmacenCreate):
     if entrada.cantidad < 1:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El campo cantidad tiene que ser mayor a 0")
     
-    validacion = db.query(ProductoModel).filter(ProductoModel.id==entrada.producto_id).first()
+    validacion = db.query(BienModel).filter(BienModel.id==entrada.producto_id).first()
     if validacion is None:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El producto no existe")
     
@@ -23,22 +24,9 @@ def c_aumentar_stock(db, entrada:ProductoAlmacenCreate):
     if validacion is None:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El almacen no existe")
     # Validaciones fin
-    print("1")
-    producto_almacen = db.query(Producto_AlmacenModel).filter(Producto_AlmacenModel.id==entrada.almacen_id).all()
-    print("2")
-    if producto_almacen != False:
-        print("3")
-        producto_almacen = db.query(producto_almacen).filter(producto_almacen.producto_id==entrada.producto_id).first()
-        print("4")
-        if producto_almacen is not None:
-            try:
-                producto_almacen.cantidad += entrada.cantidad
-                db.add(producto_almacen)
-                db.commit()
-                return True
-            except Exception as e:
-                raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error interno del servidor")
-            
+    producto_almacen = db.query(Producto_AlmacenModel).filter(Producto_AlmacenModel.almacen_id==entrada.almacen_id,
+                                                              Producto_AlmacenModel.producto_id==entrada.producto_id
+                                                              ).first()
     if producto_almacen is None:
         try:
             datos = Producto_AlmacenModel(
@@ -51,3 +39,14 @@ def c_aumentar_stock(db, entrada:ProductoAlmacenCreate):
             return True
         except Exception as e:
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error interno del servidor")
+
+    if producto_almacen is not None:
+        try:
+            producto_almacen.cantidad += entrada.cantidad
+            db.add(producto_almacen)
+            db.commit()
+            return True
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error interno del servidor")
+            
+    
